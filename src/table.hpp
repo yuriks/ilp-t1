d2f00fb060160741e9101a2dd4a59c67a956e160
brunodea <brunodea@inf.ufsm.br>
2011-10-31 23:34:17 -0200

    declaration of functions insert and lookup for the SymbolTable added.

--- src/table.hpp
+++ src/table.hpp
@@ -13,6 +13,22 @@ struct Registry {
 
 struct SymbolTable {
     std::vector<Registry> elements;
+
+    void insert(const Registry &reg);
+    Registry *lookup(const std::string &name);
 };
+
+void SymbolTable::insert(const Registry &reg)
+{
+    Registry *p = lookup(reg.name);
+    if(p == NULL)
+    {
+        elements.push_back(reg);
+    }
+    else
+    {
+        //error. registry already defined.
+    }
+}
 
 } //namespace inference
